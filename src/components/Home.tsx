5954e977bfb7ce2e9712d3320c5f8db932389989
Koji Eguchi <koji.eguchi@hotmail.com>
2023-06-06 16:29:47 -0400

    Create/style Work component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,18 +1,10 @@
 
-
-
-
 export default function Home() {
-
-  
-
-
-
   return (
     <div className="d-flex align-items-center justify-content-center vh-100">
       <div className="text-center">
-        <h1 className="">Koji Eguchi</h1>
-        <h3>Full Stack Developer</h3>
+        <h1 className="display-5">Koji Eguchi</h1>
+        <p className="fs-5">Full Stack Developer</p>
         <div className="d-flex justify-content-center gap-3">
           <a href="https://www.linkedin.com/in/kojieguchi" target="_blank"><i className="display-6 text-secondary fa-brands fa-linkedin"></i></a>
           <a href="https://github.com/Kody-Eguchi" target="_blank"><i className="display-6 text-secondary fa-brands fa-square-github"></i></a>
--- src/components/Work/Work.tsx
+++ src/components/Work/Work.tsx
@@ -1,26 +1,305 @@
-import nourishd from '../../assets/images/nourishd.png'
+import nourishd from '../../assets/images/nourishd.png';
+import quiz from '../../assets/images/quiz-app.png';
+import tweeter from '../../assets/images/tweeter-app.png';
+import scheduler from '../../assets/images/scheduler-app.png';
+import tinyApp from '../../assets/images/tiny-app.png';
+import romantech from '../../assets/images/romantech.png';
+import dishcover from '../../assets/images/dishcover.png';
+import nextjs from '../../assets/images/nextjs.png';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+import { Carousel } from 'react-responsive-carousel';
 
 function Work() {
   return (
     <div>
+      <Carousel className='w-75 mx-auto my-5 d-none d-md-block'>
+          <div>
+            <img src={nourishd} />
+            <div className="legend bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/nourishd" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-react-original"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-rails-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-postgresql-plain"></i>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={quiz} />
+            <div className="legend bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/quiz_app" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-bootstrap-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-jquery-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-postgresql-plain"></i>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={scheduler} />
+            <div className="legend bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/scheduler" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-bootstrap-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-react-original"></i>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={tinyApp} />
+            <div className="legend bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/tinyapp" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={tweeter} />
+            <div className="legend bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/tweeter" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={romantech} />
+            <div className="legend bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/romantech" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-tailwindcss-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-vuejs-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-rails-plain"></i>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={nextjs} />
+            <div className="legend bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/wikipedia-app" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-bootstrap-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-typescript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-nodejs-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-express-original"></i>
+              </div>
+            </div>
+          </div>
+          <div>
+            <img src={dishcover} />
+            <div className="legend bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/Dishcover" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-tailwindcss-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-vuejs-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-rails-plain"></i>
+              </div>
+            </div>
+          </div>
+      </Carousel>
 
-      <div>
-        <img src={nourishd} alt="" />
-        <div>
-
+      <div className='d-flex d-md-none w-75 mx-auto mt-4 mb-5 flex-column '>
+        <div className='text-center d-flex flex-column p-3'>
+            <img src={nourishd} className='w-100'/>
+            <div className="d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/nourishd" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-react-original"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-rails-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-postgresql-plain"></i>
+              </div>
+            </div>
+            <hr/>
         </div>
-        <div>
-          <a href="https://github.com/Kody-Eguchi" target="_blank"><i className="display-6 text-secondary fa-brands fa-square-github"></i></a>
-          <i className="fa-solid fa-browser"></i>
+        
+        <div className='text-center d-flex flex-column p-3'>
+            <img src={quiz} className='w-100'/>
+            <div className="d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/quiz_app" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-bootstrap-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-jquery-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-postgresql-plain"></i>
+              </div>
+            </div>
+            <hr/>
+          </div>
+          <div className='text-center d-flex flex-column p-3'>
+            <img src={scheduler} className='w-100'/>
+            <div className="bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/scheduler" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-bootstrap-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-react-original"></i>
+              </div>
+            </div>
+            <hr/>
+          </div>
+          <div className='text-center d-flex flex-column p-3'>
+            <img src={tinyApp} className='w-100'/>
+            <div className="bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/tinyapp" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+              </div>
+            </div>
+            <hr/>
+          </div>
+          <div className='text-center d-flex flex-column p-3'>
+            <img src={tweeter} className='w-100'/>
+            <div className="bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/tweeter" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+              </div>
+            </div>
+            <hr/>
+          </div>
+          <div className='text-center d-flex flex-column p-3'>
+            <img src={romantech} className='w-100'/>
+            <div className="bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/romantech" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-tailwindcss-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-vuejs-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-rails-plain"></i>
+              </div>
+            </div>
+            <hr/>
+          </div>
+          <div className='text-center d-flex flex-column p-3'>
+            <img src={nextjs} className='w-100'/>
+            <div className="bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/wikipedia-app" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-bootstrap-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-typescript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-nodejs-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-express-original"></i>
+              </div>
+            </div>
+            <hr/>
+          </div>
+          <div className='text-center d-flex flex-column p-3'>
+            <img src={dishcover} className='w-100'/>
+            <div className="bg-white d-flex flex-column gap-3 pb-4">
+              <div>
+                <p className='text-dark fs-5'>LINKS</p>
+                <a href="https://github.com/Kody-Eguchi/Dishcover" target="_blank"><i className="fs-1 text-secondary fa-brands fa-square-github"></i></a>
+              </div>
+              <div>
+              <p className='text-dark fs-5'>Tech Stack</p>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-html5-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-css3-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-tailwindcss-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-javascript-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-vuejs-plain"></i>
+                <i className="mx-1 fs-5 text-white bg-secondary rounded p-2 devicon-rails-plain"></i>
+              </div>
+            </div>
+          </div>
+          <hr/>
         </div>
-      </div>
-
-
-
-
-
-
-
 
     </div>
   )
